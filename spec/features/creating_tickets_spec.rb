609588df189a930645e85b33a894d7361e9af3ae
Marc <marc.bluemner@gmail.com>
2014-11-13 14:59:41 +0100

    Implement creating tickets for a project

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -5,16 +5,27 @@ feature "Creating Tickets" do
     visit '/'
     click_link "Internet Explorer"
     click_link "New Ticket"
-end
+  end
+
   scenario "Creating a ticket" do
     fill_in "Title", with: "Non-standards compliance"
     fill_in "Description", with: "My pages are ugly!"
     click_button "Create Ticket"
     expect(page).to have_content("Ticket has been created.")
   end
+
   scenario "Creating a ticket without valid attributes fails" do
     click_button "Create Ticket"
     expect(page).to have_content("Ticket has not been created.")
     expect(page).to have_content("Title can't be blank")
     expect(page).to have_content("Description can't be blank")
-end end
+  end 
+
+  scenario "Description must be longer than 10 characters" do
+    fill_in "Title", with: "Non-standards compliance"
+    fill_in "Description", with: "it sucks"
+    click_button "Create Ticket"
+    expect(page).to have_content("Ticket has not been created.")
+    expect(page).to have_content("Description is too short")
+  end
+end
