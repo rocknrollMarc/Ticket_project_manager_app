959a2a73c9daefa1c9c0e0314c0174c7a6fe6b98
Marc <marc.bluemner@gmail.com>
2014-11-13 14:03:54 +0100

    project can not be updated without a name

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,15 +23,19 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find(params[:id])
+  @project = Project.find(params[:id])
   end
 
-  def update
-    @project = Project.find(params[:id])
-    @project.update(project_params)
+def update
+  @project = Project.find(params[:id])
+  if @project.update(project_params)
     flash[:notice] = "Project has been updated."
     redirect_to @project
+  else
+    flash[:alert] = "Project has not been updated."
+    render "edit"
   end
+end
 
   private
 
